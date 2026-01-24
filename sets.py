cca7f0e7000097046cc0049ee99a0ca930f1351c
Prashant Sapkota <prashantsapkota148@gmail.com>
2026-01-24 14:43:40 +0545

    Add files via upload

---
+++ sets.py
@@ -0,0 +1,9 @@
+s ={} #it creates empty dictionary not sets
+A =set() # set() else it will create a tupple
+print(type(s))
+print(type(A))
+
+f = {1,4,5,6,6,8,"ram"}
+print(f)
+f.add("Ram")
+print(f)
