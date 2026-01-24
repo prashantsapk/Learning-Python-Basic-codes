79e80e3419178ad708bf6f3253895e8c589d5e55
Prashant Sapkota <prashantsapkota148@gmail.com>
2026-01-24 09:16:33 +0545

    Add files via upload

---
+++ Tuple.py
@@ -0,0 +1,5 @@
+# Tupple are immutable
+a = ("prashant",5,6,8,8)
+print("The tupple is ",a)
+print("Counting how many 8 we have",a.count(8)) #count how many 8 we have
+print("Using index",a.index(8)) # on which index the 8 value is ? its 3.
