cc3d50e9f1887cf49c2d7f083cf24286aa848f47
Prashant Sapkota <prashantsapkota148@gmail.com>
2026-01-25 20:07:48 +0545

    Add files via upload

---
+++ present in a list or not.py
@@ -0,0 +1,7 @@
+# Write a program which finds out whether a given name is present in a list or not. 
+a = input("Enter your name")
+list =["Prashant","ram","albert"]
+if(a in list):
+    print(f"{a} is in the list")
+else:
+    print(f"{a} is not in the list")
