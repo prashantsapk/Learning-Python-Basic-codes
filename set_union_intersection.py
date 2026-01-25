599fd7116fd385b865f742326a0ae6fe0d594885
Prashant Sapkota <prashantsapkota148@gmail.com>
2026-01-25 07:55:36 +0545

    Add files via upload

---
+++ set_union_intersection.py
@@ -0,0 +1,6 @@
+s = {1,4,5,6,7}
+s1= {1,4,8,9}
+
+b=s.union(s1) # you can store value like this
+print("The union of two sets is ",b) # printing union of two sets
+print("The intersection of two set is",s.intersection(s1)) # printing intersection
