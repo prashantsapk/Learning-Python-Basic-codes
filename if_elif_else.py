dfb838b1f44eeb23dd8019b43f96a14303039475
Prashant Sapkota <prashantsapkota148@gmail.com>
2026-01-25 09:04:19 +0545

    Add files via upload

---
+++ if_elif_else.py
@@ -0,0 +1,9 @@
+# learning if else if
+
+a= int(input("Enter your age"))
+if (a<18):
+    print("your age not eligible to vote")
+elif(a>=18):
+    print("you can vote")
+else:
+    print("You entered invalid age")
