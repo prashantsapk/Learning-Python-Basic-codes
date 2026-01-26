77ac91e97bceee8225f621c54817cf8ca66df88a
Prashant Sapkota <prashantsapkota148@gmail.com>
2026-01-26 08:56:36 +0545

    Add files via upload

---
+++ break continue & pass statement.py
@@ -0,0 +1,13 @@
+i=0
+for i in range(0,5,1):
+    if(i==2):
+        break #exits the entire loop
+    print(i)
+
+for i in range(0,5,1):
+    if(i==2):
+        continue # skips the current iteration ( prints the reamning all just skips current iteration)
+    print(i)
+
+for i in range(0,5,1):
+    pass # do nothing
