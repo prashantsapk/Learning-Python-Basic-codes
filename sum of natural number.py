8497db8bb50bb0e25a7691d1d8114d74002b6f0e
Prashant Sapkota <prashantsapkota148@gmail.com>
2026-01-27 21:39:02 +0545

    Update sum calculation to use while loop
    Refactor the program to calculate the sum of first n natural numbers using a while loop instead of a for loop.

--- sum of natural number.py
+++ sum of natural number.py
@@ -1,6 +1,13 @@
 # Write a program to find the sum of first n natural numbers using while loop. 
-a = int(input("Enter the number of how many natural number sum you want"))
-for i in range(1,a+1,1):
-    a+=1
+n = int(input("Enter the number of how many natural number sum you want"))
 
-print(a)
+# should use while loop
+# it should be like for example 5, 1 + 2 + 3 + 4 + 5
+i =1
+j=0
+
+while(i<=n):
+   j +=  i
+   i+=1
+   
+print(j)
