87ed9e00cb59cb47b782972dd0a09508405a0750
Prashant Sapkota <prashantsapkota148@gmail.com>
2026-02-14 15:28:46 +0545

    Add files via upload

---
+++ match_case.py
@@ -0,0 +1,20 @@
+# its same like switch case we used in c programming
+
+a = int(input("Enter the number of days from 1 to 7 anyone you like"))
+
+match a:
+    case 1:
+        print("The day is Sunday ")
+    case 2:
+        print("The day is Monday ")
+    case 3:
+        print("The day is Tuesday ")
+    case 4:
+        print("The day is Wednesday ")
+    case 5:
+        print("The day is Thursday ")
+    case 6:
+        print("The day is Friday")
+    case 7:
+        print("The day is  Saturday")
+    
