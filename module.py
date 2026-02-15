a194d2bfae5cfb7b3a9b718c88f5fd8141412419
Prashant Sapkota <prashantsapkota148@gmail.com>
2026-02-15 07:15:53 +0545

    Add main guard to module.py
    Added main guard to execute printname function.

--- module.py
+++ module.py
@@ -1,5 +1,11 @@
 def printname():
     print("HELLO WORLD")
 
-printname()
-print(__name__)
+
+if __name__ == "__main__": # the code that doesn't need to be executed
+#     # for another file or when the another is running we use this
+#     # because __name__="__main__" is only for current file this file
+#     # other wise if runned in another file it will be __name__ = "module"
+ printname()
+ print(__name__)
+
