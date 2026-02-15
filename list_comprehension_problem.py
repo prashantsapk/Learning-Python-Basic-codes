c7508e6aea516373b933ebf7b1bb2d41131c4cb5
Prashant Sapkota <prashantsapkota148@gmail.com>
2026-02-15 15:11:52 +0545

    Add files via upload

--- list_comprehension_problem.py
+++ list_comprehension_problem.py
@@ -1,16 +1,12 @@
 # 3. Write a list comprehension to print a list which contains the multiplication table of a
 # user entered number.
 
-a = int(input("Enter the number which multiplication table you want"))
-list1=[]
-
-for i in range(1,11,1):
-    list1.append(a*i)
-
-print(list1)  # normmal way to that using list
-
 
 # using list comprehension 
+a=int(input("Enter the number of which multiplication you want"))
 multiplicationtable1=[1,2,3,4,5,6,7,8,9,10]
 multiplicationtable2=[a*i for i in  multiplicationtable1 ]
-print(multiplicationtable2)
+# print(multiplicationtable2)
+
+with (open("Tables.txt","a")as f1):
+    f1.write(f"{multiplicationtable2}\n")
