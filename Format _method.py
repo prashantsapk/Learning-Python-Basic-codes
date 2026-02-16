880f58baff71de279ca03d142bd952e7ca9be4aa
Prashant Sapkota <prashantsapkota148@gmail.com>
2026-02-16 11:05:00 +0545

    Add files via upload

---
+++ Format _method.py
@@ -0,0 +1,17 @@
+# FORMAT METHOD (STRINGS)
+# Formats the values inside the string into a desired output.
+# template.format(p1,p2...)
+# Syntax:
+# "{} is a good {}".format("harry", "boy") #1.
+# "{} is a good {o}".format("harry", "boy") #2.
+# # output for 1:
+# # harry is a good boy
+# # output for 2:
+# # boy is a good harry
+
+
+
+
+print("{} is a nice person {}".format("Prashant","ok"))
+print("{1} is a nice person {0}".format("Prashant","ok")) # you can use number as index
+
