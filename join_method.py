bed6a98ac8811495b11c245acad4faf44b3f3378
Prashant Sapkota <prashantsapkota148@gmail.com>
2026-02-16 10:19:23 +0545

    Add files via upload

---
+++ join_method.py
@@ -0,0 +1,13 @@
+# JOIN METHOD (STRINGS)
+# Creates a string from iterable objects.
+# l = ["apple", "mango", "banana"]
+# result = ", and, ".join(l)
+# print(result)
+
+list1=["apple","mango","banana"]
+result =", and ,".join(list1)
+print(result)
+
+list3=["Prashant","Bananaa","nepal","india"]
+list4=" space ".join(list3) # basically this shit joins after each one index, simple means Prashant space Banana space nepal space india
+print(list4)
